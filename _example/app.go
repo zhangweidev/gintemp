882f1899ad595de5148ea6d81ae88c5270fa8020
张巍 <zhangwei@3laoshi.com>
2020-09-23 15:16:51 +0800

    feat: template obj

---
+++ _example/app.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/xxiu/gintemp"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.HTMLRender = gintemp.LoadTemplates()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "home/index.html", gin.H{})
+	})
+
+	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
